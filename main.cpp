b3bab64b0d635d48bab4c0254d5888094c7fec25
renkeles <84261162+renkeles@users.noreply.github.com>
2021-09-07 10:45:53 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,55 +1,153 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include "ArrayInt.h"
+#ifndef ARRAYINT_H
+#define ARRAYINT_H
 
-void task1(){
-    ArrayInt arrayInt;
-    std::cout << arrayInt.getLength() << std::endl;
-    for(int i = 0; i < 10; i++){
-        arrayInt.push_back(1 + rand()%10);
-    }
-    std::cout << arrayInt.getLength() << std::endl;
-    arrayInt.print();
-    arrayInt.sort();
-    arrayInt.print();
+#include <cassert>
 
-    for(int i = 0; i < 2; ++i){
-        std::cout << "First item = "<<arrayInt.pop_front() << std::endl;
-        arrayInt.print();
+class ArrayInt
+{
+private:
+    int m_length;
+    int* m_data;
+
+    void swap(int* a, int* b) {
+        int tmp = *a;
+        *a = *b;
+        *b = tmp;
     }
 
-    for(int i = 0; i < 2; ++i){
-        std::cout << "Last item = "<<arrayInt.pop_back() << std::endl;
-        arrayInt.print();
+public:
+    ArrayInt() : m_length(0), m_data(nullptr)
+    { }
+
+    ArrayInt(int length) :
+        m_length(length)
+    {
+        assert(length >= 0);
+
+        if (length > 0)
+            m_data = new int[length];
+        else
+            m_data = nullptr;
     }
-}
 
-void task2(){
-    std::vector<int> array;
-    for(int i = 0; i < 10; i++){
-        array.push_back(1 + rand() % 10);
+    ~ArrayInt()
+    {
+        delete[] m_data;
     }
-    for(std::vector<int>::const_iterator i = array.begin(); i != array.end(); ++i){
-        std::cout << *i << " ";
+
+    void erase()
+    {
+        delete[] m_data;
+
+        m_data = nullptr;
+        m_length = 0;
     }
-    std::cout << std::endl;
-    std::sort(array.begin(), array.end());
+    int getLength() { return m_length; }
 
-    for(std::vector<int>::const_iterator i = array.begin(); i != array.end(); ++i){
-        std::cout << *i << " ";
+
+    int& operator[](int index)
+    {
+        assert(index >= 0 && index < m_length);
+        return m_data[index];
     }
-    std::cout << std::endl;
-    int result = std::unique(array.begin(),array.end()) - array.begin();
-    std::cout << result << std::endl;
 
-}
+    void resize(int newLength)
+    {
+        if (newLength == m_length)
+            return;
 
-int main() {
+        if (newLength <= 0)
+        {
+            erase();
+            return;
+        }
 
-    //task1();
-    //task2();
+        int* data = new int[newLength];
+
+        if (m_length > 0)
+        {
+            int elementsToCopy = (newLength > m_length) ? m_length : newLength;
+
+            for (int index = 0; index < elementsToCopy; ++index)
+                data[index] = m_data[index];
+        }
+
+        delete[] m_data;
+
+        m_data = data;
+        m_length = newLength;
+    }
+
+    void insertBefore(int value, int index)
+    {
+        assert(index >= 0 && index <= m_length);
+
+        int* data = new int[m_length + 1];
+
+        for (int before = 0; before < index; ++before)
+            data[before] = m_data[before];
+
+        data[index] = value;
+
+        for (int after = index; after < m_length; ++after)
+            data[after + 1] = m_data[after];
+
+        delete[] m_data;
+        m_data = data;
+        ++m_length;
+    }
+
+    void push_back(int value) { insertBefore(value, m_length); }
+
+    void print() {
+        for (int i = 0; i < m_length; i++) {
+            std::cout << m_data[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void sort() {
+        for (int i = 1; i < m_length; i++) {
+            int k = i;
+            while (k > 0 && m_data[k - 1] < m_data[k]) {
+                swap(&m_data[k], &m_data[k - 1]);
+                k -= 1;
+            }
+        }
+    }
+
+    int pop_front() {
+        if (m_length == 0) return 0;
+        int tmp = m_data[0];
+        int* data = new int[m_length - 1];
+
+        for (int i = 1; i < m_length; ++i) {
+            data[i - 1] = m_data[i];
+        }
+        
+        delete[] m_data;
+        m_data = data;
+        --m_length;
+        return tmp;
+    }
+
+    int pop_back() {
+        if (m_length == 0) return 0;
+        int tmp = m_data[m_length - 1];
+        int* data = new int[m_length - 1];
+
+        for (int i = 0; i < m_length - 1; ++i) {
+            data[i] = m_data[i];
+        }
+
+        delete[] m_data;
+        m_data = data;
+        --m_length;
+        return tmp;
+    }
+
+};
 
 
-    return 0;
-}
+
+#endif
